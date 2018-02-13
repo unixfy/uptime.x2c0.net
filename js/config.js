bda5ef142c8f60808c4d10102308e30104658f7d
Alex Wang <admin@x2c0.net>
2018-02-12 20:56:05 -0500

    Update API Configs

--- js/config.js
+++ js/config.js
@@ -5,7 +5,6 @@ var __apiKeys = [
 	'm779993726-bf8f9d3b0056f04dfb5398ca', // Charoite-ORD
 	'm779993724-cb2cd426d7eef44329788a39', // Diopside-DFW
 	'm779993734-f652316f484c4f0bd94ff994', // C9IDE
-	'm779993732-167adb42423b9c5c53e8f916', // Remote Desk
 ];
 
 // refresh interval (in seconds)
